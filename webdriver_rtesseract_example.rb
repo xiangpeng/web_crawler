fda6316e681b8ebc62519737f58ecb866919e253
xiangpeng <koalaxp8@gmail.com>
2015-09-23 17:27:01 +0800

    v0.5

--- webdriver_rtesseract_example.rb
+++ webdriver_rtesseract_example.rb
@@ -4,6 +4,7 @@ require 'bundler/setup'
 
 require 'rtesseract'
 require 'mini_magick'
+require 'mechanize'
 require 'watir-webdriver'
 require 'watir/extensions/element/screenshot'
 require 'securerandom'
@@ -20,12 +21,52 @@ def parse_number(number_url)
 end
 
 # browser = Watir::Browser.new :phantomjs
-browser = Watir::Browser.new :chrome
-browser.goto 'http://zhixing.court.gov.cn/search/'
-file_name = "#{SecureRandom.uuid}.jpg"
-browser.img(id: 'captchaImg').screenshot(file_name)
-cap_str = parse_number(file_name).gsub(/\s/,'')
-browser.text_field(id: 'cardNum').set '33032519540116531X'
-browser.text_field(id: 'j_captcha').set cap_str
-browser.button(id: 'button').click
-browser.iframe(index: 0).table(id: 'Resultlist')
+
+
+class CourtCrawler
+  attr_accessor :browser, :data_agent
+  def initialize
+    @data_agent = Mechanize.new
+    @data_agent.user_agent = "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/41.0.2227.0 Safari/537.36"
+    @browser = Watir::Browser.new :chrome
+    @browser.goto 'http://zhixing.court.gov.cn/search/'
+  end
+
+  def crawl(cert_no)
+    5.times do
+      file_name = "./temp_img/#{SecureRandom.uuid}.jpg"
+      @browser.img(id: 'captchaImg').screenshot(file_name)
+      cap_str = parse_number(file_name).gsub(/\s/,'')
+      @browser.text_field(id: 'cardNum').set cert_no
+      @browser.text_field(id: 'j_captcha').set cap_str
+      @browser.button(id: 'button').click
+      if @browser.alert.exists?
+        @browser.alert.ok
+        sleep 1
+        next
+      else
+        rows = @browser.iframe(index: 0).table(id: 'Resultlist').rows
+        puts rows.count
+        if(rows.count > 1)
+          rows[1..-1].each do |row|
+            puts get_detail "http://zhixing.court.gov.cn/search/detail?id=#{row.links[-1].id}"
+          end
+        end
+        break
+      end
+    end
+  end
+
+  def get_detail(url)
+    page = @data_agent.get url
+    return JSON.parse page.body
+  end
+
+  def close
+    @browser.quit
+  end
+end
+
+crawler = CourtCrawler.new
+crawler.crawl 'xxxxxxxxxxxx'
+crawler.close
