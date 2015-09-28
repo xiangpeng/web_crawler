cf8b771942813b8d3e33ad892a069c7fe79028bd
XiangPeng <koalaxp8@gmail.com>
2015-09-28 20:05:59 +0800

    v0.99

--- court_crawler1.rb
+++ court_crawler1.rb
@@ -4,16 +4,20 @@ require 'digest/md5'
 require 'rest-client'
 require 'nokogiri'
 require './model'
-ActiveRecord::Base.establish_connection(:adapter => "postgresql", :database  => "crawler_db", host: "localhost", user_name: 'xiangpeng')
+ActiveRecord::Base.establish_connection(:adapter => "postgresql", :database  => "crawler_db", host: "localhost", user_name: 'xiangpeng', pool: 100)
 
 class CourtCrawler
   def crawl(cert_no)
-    html_str = RestClient.post "http://zhixing.court.gov.cn/search/search", {searchCourtName:'全国法院（包含地方各级法院）',selectCourtId:1,selectCourtArrange:1,pname:"",cardNum:cert_no,j_captcha:'99999'}, user_agent: 'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/41.0.2227.0 Safari/537.36'
+    begin
+      html_str = RestClient.post "http://zhixing.court.gov.cn/search/search", {searchCourtName:'全国法院（包含地方各级法院）',selectCourtId:1,selectCourtArrange:1,pname:"",cardNum:cert_no,j_captcha:'99999'}, user_agent: 'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/41.0.2227.0 Safari/537.36'
+    rescue RestClient::ResourceNotFound => e
+      return
+    end
     html_doc = Nokogiri::HTML(html_str)
     html_doc.css("#Resultlist a:contains('查看')").each do |i|
       id = i.attr('id')
       item = get_detail  "http://zhixing.court.gov.cn/search/detail?id=#{id}"
-      puts item
+      puts "#{cert_no}:#{item}"
       md5 = Digest::MD5.hexdigest("#{item[:name]}#{item[:ourt_name]}#{item[:create_time]}#{item[:case_code]}#{item[:case_state]}#{item[:exec_money]}")
       puts md5
       CourtExecInfo.create(item.merge(cert_no: cert_no, crawl_date: Date.current, md5: md5))
--- run_task.rb
+++ run_task.rb
@@ -5,5 +5,5 @@ require './model'
 ActiveRecord::Base.establish_connection(:adapter => "postgresql", :database  => "crawler_db", host: "localhost", user_name: 'xiangpeng')
 cert_no_arr = CustInfo.pluck(:cert_no).in_groups(4)
 cert_no_arr.each_with_index do |arr, index|
-  (arr.compact).in_groups_of(25).each {|i| Sidekiq::Client.push 'class'=> 'CrawlerWorker', 'args'=> [i.compact], 'queue'=> "crawler#{index}" }
+  (arr.compact).in_groups_of(500).each {|i| Sidekiq::Client.push 'class'=> 'CrawlerWorker', 'args'=> [i.compact], 'queue'=> "crawler#{index}" }
 end
